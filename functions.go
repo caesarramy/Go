17af9ebb1c0552913d2495fc0d4842e2e19a6bc4
caesarramy <ramygeorge7@gmail.com>
2021-01-30 19:11:34 +0200

    Create functions.go

---
+++ functions.go
@@ -0,0 +1,21 @@
+// A function is a block of code that performs a specific task. A function takes a input, performs some calculations on the input and generates a output.
+// func functionname(parametername type) returntype {  
+//function body
+// }
+/*
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Total Price", calculateBill(5, 2))
+}
+
+func calculateBill(price int, no int) int {
+	totalprice := price * no
+	return totalprice
+}
+
+*/
+
