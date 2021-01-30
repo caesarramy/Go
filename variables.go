19f106084d2496a46ea39432355f7c04be93f637
caesarramy <ramygeorge7@gmail.com>
2021-01-30 17:28:07 +0200

    Update variables.go

--- variables.go
+++ variables.go
@@ -1,9 +1,9 @@
 // Variable is the name given to a memory location to store a value of a specific type. There are various syntaxes to declare variables in Go. Let's look at them one by one.
 // How to declare variable ?
 // var name type as example => var age int
-// if int is not assigned with value by default it will be zero
+// if int is not assigned with value by default it will be zero to test it uncomment /*   *\
 
-package main
+/* package main
 
 import "fmt"
 
@@ -11,3 +11,21 @@ func main() {
 	var age int // variable declaration
 	fmt.Println("My age is", age)
 }
+
+*/
+
+// A variable can be assigned to any value of its type. In the above program, age can be assigned any integer value.
+
+/*
+
+func main() {
+
+	var age int // variable declaration
+	fmt.Println("My age is", age)
+	age = 29
+	fmt.Println("My age is", age)
+	age = 40
+	fmt.Println("My new age is", age)
+}
+
+*/
