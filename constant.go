264a2a1c23b7174a39fa3459651589c20b987375
caesarramy <ramygeorge7@gmail.com>
2021-01-30 18:31:02 +0200

    Update constant.go

--- constant.go
+++ constant.go
@@ -8,3 +8,17 @@ func main() {
 }
 
 */
+
+/*
+func main() {  
+    const (
+        name = "John"
+        age = 50
+        country = "Canada"
+    )
+    fmt.Println(name)
+    fmt.Println(age)
+    fmt.Println(country)
+
+}
+*/
