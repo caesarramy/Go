1ab29a4b84c48ff645270f21c38854b35b76ac7e
caesarramy <ramygeorge7@gmail.com>
2021-01-30 17:13:38 +0200

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -3,11 +3,13 @@
 // if you want to build the code only 
 // go build helloworld.go it will produce file helloworld.exe or main file  in linux or mac os  you can execute it by .\helloworld.exe or .\helloworld
 
-package main
+package main // Every go file must start with the package name statement , Packages are used to provide code compartmentalization and reusability
+             // اى مشروع جديد لازم يكون فية بكدج كانك بتجمع كل الكود فى حاجة واحدة مش شرط يكون اسم ال بكدج main
+            // اى حاجة هيتعملها Run لازم هتكون ف func  ال main
 
-import "fmt"
+import "fmt" // دى library بتعمل println 
 
-func main() {
+func main() { // اى func مهمة بيتعمل ليها print هنا 
 
 	fmt.Println("Hello World")
   
