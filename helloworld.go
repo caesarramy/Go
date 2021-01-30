e3a6046b1cde306fc4c678c3139bba1a6d8ca555
caesarramy <ramygeorge7@gmail.com>
2021-01-30 17:07:03 +0200

    Update and rename main.go to helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,14 @@
+// Run this file by 
+// go run helloworld.go it will run the program from editor 
+// if you want to build the code only 
+// go build helloworld.go it will produce file helloworld.exe or main file  in linux or mac os  you can execute it by .\helloworld.exe or .\helloworld
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World")
+  
+}
